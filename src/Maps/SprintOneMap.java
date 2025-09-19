0a5bbd57d6a4dd48dfeb50ce3c681222c17bdd89
Wilson Chen <wilson.chen@quinnipiac.edu>
2025-09-19 19:37:19 -0400

    Added a new map, created a new screen for the first map, modified the game level to show the new map, and increased sprite speed for exploration of the new map.

--- src/Maps/SprintOneMap.java
+++ src/Maps/SprintOneMap.java
@@ -5,21 +5,37 @@ import Engine.ImageLoader;
 import GameObject.ImageEffect;
 import GameObject.Sprite;
 import Level.Map;
+import Scripts.SimpleTextScript;
+import Scripts.TestMap.TreeScript;
 import Tilesets.CommonTileset;
 import Utils.Colors;
 import Utils.Point;
 
 public class SprintOneMap extends Map {
 
-    private Sprite cat;
 
     public SprintOneMap() {
         super("sprint_one_map.txt", new CommonTileset());
-        
+        this.playerStartPosition = getMapTile(63, 63).getLocation();
     }
 
     @Override
     public void draw(GraphicsHandler graphicsHandler) {
         super.draw(graphicsHandler);
+    }
+
+    @Override
+    public void loadScripts() {
+
+        // simple signs and markers to indicate the relative locations of the Hive and other regions
+        getMapTile(63, 63).setInteractScript(new SimpleTextScript("The Hive"));
+
+        getMapTile(0, 0).setInteractScript(new SimpleTextScript("(0,0) Arctic Region"));
+
+        getMapTile(124, 0).setInteractScript(new SimpleTextScript("(124, 0) Arctic Region?"));
+
+        getMapTile(124, 124).setInteractScript(new SimpleTextScript("(124, 124) Grassy Region?"));
+
+        getMapTile(0, 124).setInteractScript(new SimpleTextScript("(0, 124) Volcanic Region?"));
     }
 }
