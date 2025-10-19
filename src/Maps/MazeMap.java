7d9cef1a7e9c24defee2c5138f57703d860bacec
Wilson Chen <wilson.chen@quinnipiac.edu>
2025-10-18 23:11:55 -0400

    Added and read the spritesheet for the maze map.

--- src/Maps/MazeMap.java
+++ src/Maps/MazeMap.java
@@ -3,11 +3,12 @@ package Maps;
 import Level.Map;
 import Level.Tileset;
 import Tilesets.CommonTileset;
+import Tilesets.MazeTileset;
 
 public class MazeMap extends Map {
 
     public MazeMap() {
-        super("maze_map.txt", new CommonTileset());
+        super("maze_map.txt", new MazeTileset());
         this.playerStartPosition = getMapTile(17, 20).getLocation();
     }
     
