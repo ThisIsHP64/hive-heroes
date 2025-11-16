8260d0f580b0a905f6d6d81154f808ecda4cdd99
Wilson Chen <wilson.chen@quinnipiac.edu>
2025-11-16 14:24:18 -0500

    Fixed up the Queen Bee's script and the introductory script.

--- src/Screens/GrassLevelScreen.java
+++ src/Screens/GrassLevelScreen.java
@@ -43,20 +43,20 @@ public class GrassLevelScreen extends Screen implements GameListener {
     public void initialize() {
         hasInitialized = true;
         flagManager = new FlagManager();
-        flagManager.addFlag("isLevel1", true);
-        flagManager.addFlag("isLevel2", true);
-        flagManager.addFlag("isLevel3", true);
-        flagManager.addFlag("isLevel4", true);
+        // flagManager.addFlag("isLevel1", true);
+        // flagManager.addFlag("isLevel2", true);
+        // flagManager.addFlag("isLevel3", true);
+        // flagManager.addFlag("isLevel4", true);
 
         flagManager.addFlag("hasCollected15Nectar", false);
         flagManager.addFlag("hasCollected50Nectar", false);
         flagManager.addFlag("hasCollected100Nectar", false);
         flagManager.addFlag("hasCollected150Nectar", false);
 
-        // flagManager.addFlag("isLevel1", false);
-        // flagManager.addFlag("isLevel2", false);
-        // flagManager.addFlag("isLevel3", false);
-        // flagManager.addFlag("isLevel4", false);
+        flagManager.addFlag("isLevel1", false);
+        flagManager.addFlag("isLevel2", false);
+        flagManager.addFlag("isLevel3", false);
+        flagManager.addFlag("isLevel4", false);
 
         map = new GrassMap();
         map.setFlagManager(flagManager);
